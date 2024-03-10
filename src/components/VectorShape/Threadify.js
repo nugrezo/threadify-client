68b5d110ee4b9d91cf13f07af112b53df5aaf8b7
Ergun Oz <ergun.oz82@gmail.com>
2024-03-09 21:54:50 -0500

    landing page style is changed

--- src/components/VectorShape/Threadify.js
+++ src/components/VectorShape/Threadify.js
@@ -6,10 +6,10 @@ const Threadify = () => {
     <div className="threadify-container">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 200 200"
+        viewBox="0 40 200 200"
         className="vector-shape"
       >
-        <path d="M100,100 m0,-50 a50,50 0 1,1 0,100 a50,50 0 1,1 0,-100" />
+        <path d="M50,400 Q75,50 100,100 T150,100 Q175,50 200,100 T250,100 Q275,50 300,100 T350,100 Q375,50 400,100 T450,100" />
         <text
           x="50%"
           y="50%"
@@ -25,7 +25,7 @@ const Threadify = () => {
         viewBox="0 0 200 200"
         className="vector-shape"
       >
-        <path d="M100,100 m0,-50 a50,50 0 1,1 0,100 a50,50 0 1,1 0,-100" />
+        <path d="M150,100 Q75,50 100,100 T150,100 Q175,50 200,100 T250,100 Q275,50 300,100 T450,100" />
         <text
           x="50%"
           y="50%"
@@ -38,12 +38,12 @@ const Threadify = () => {
       </svg>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 200 200"
+        viewBox="0 -40 200 200"
         className="vector-shape"
       >
-        <path d="M100,100 m0,-50 a50,50 0 1,1 0,100 a50,50 0 1,1 0,-100" />
+        <path d="M50,-100 Q75,50 100,100 T150,100 Q175,50 200,700 T450,100 Q275,50 300,100 T350,100" />
         <text
-          x="50%"
+          x="40%"
           y="50%"
           dominantBaseline="middle"
           textAnchor="middle"
