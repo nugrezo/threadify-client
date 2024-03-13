80459738b03eb1b7781bc460ba7510d5ddd89c17
Ergun Oz <ergun.oz82@gmail.com>
2024-03-12 23:59:58 -0400

    api config

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl;
 const apiUrls = {
-  production: "https://threadify-server1.onrender.com",
+  production: "https://threadify-server-last.onrender.com",
   development: "http://localhost:4741",
 };
 
