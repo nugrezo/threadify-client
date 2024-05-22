29f0f56d844179745e3d3fb3617f182dbd53effb
Ergun Oz <ergun.oz82@gmail.com>
2024-05-22 09:49:00 -0400

    About app and SignUp component design updated

--- src/components/AboutApp/AboutApp.js
+++ src/components/AboutApp/AboutApp.js
@@ -9,57 +9,95 @@ const AboutApp = () => {
     <div>
       <h2>About Threadify</h2>
       <div className="about-app-container">
-        <p>
-          Threadify is a social media app designed for seamless thread creation,
-          reading, liking, and commenting. To fully engage with the apps
-          features, users are required to create an account.
-        </p>
+        <div className="what-is-threadfy">
+          Welcome to <strong>Threadify</strong>, where vibrant discussions come
+          alive through seamless thread interactions. Explore diverse topics,
+          share your thoughts, and connect with like-minded individuals in a
+          refreshing social media experience
+        </div>
+        <div>
+          <h5>Features</h5>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#e67700"
+            className="account-creation"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+            />
+          </svg>
 
-        <p>
-          <strong>Key features of Threadify</strong>
-        </p>
-        <ul>
-          <li>
-            <strong>Account Creation: </strong>Users must create an account to
-            access Threadify&apos;s features, ensuring personalized interactions
-            and data security.
-          </li>
-          <li>
-            <strong>Creating Threads:</strong> Users can easily initiate their
-            own threads on various topics, expressing their thoughts, opinions,
-            and interests to the Threadify community.
-          </li>
-          <li>
-            <strong>Reading Threads:</strong> TThreadify provides a seamless
-            browsing experience, allowing users to explore and read threads
-            created by other users, fostering engagement and knowledge-sharing.
-          </li>
-          <li>
-            <strong>Displaying Your Own Thread:</strong>Users have a dedicated
-            space to showcase threads they have created, making it easy to
-            manage and track their contributions to the platform.
-          </li>
-          <li>
-            <strong>Updating Thread:</strong> Users can edit and update their
-            own threads as needed, ensuring accuracy and relevance over time.
-          </li>
-          <li>
-            <strong>Deleting Thread:</strong> Threadify allows users to remove
-            threads they no longer wish to be displayed, providing control over
-            their content and contributions.
-          </li>
-          <li>
-            <strong>Thread Interaction:</strong> Users can engage in vibrant
-            discussions by commenting on threads created by others, sharing
-            their perspectives and insights.Threadify enables users to express
-            appreciation for threads they enjoy by liking them, fostering a
-            sense of community and recognition among users.
-          </li>
-        </ul>
-        <p className="join-now">
-          Join the Threadify community today and discover a new way to
-          experience social media!
-        </p>
+          <p>
+            Users can create a secure account before interacting with other
+            users. To address utmost security concerns, user accounts are
+            encrypted using passport and crypto technology.
+          </p>
+          <p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="#e67700"
+              className="account-creation"
+              width="50"
+              height="50"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            Users can easily create their own threads on various topics,
+            expressing their thoughts, opinions, and interests to the Threadify
+            community.
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#e67700"
+            className="account-creation"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"
+            />
+          </svg>
+          <p>
+            Users can engage with others&#39; threads, allowing them to like and
+            comment on posts, fostering vibrant discussions and community
+            interaction.
+          </p>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#e67700"
+            className="account-creation"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75"
+            />
+          </svg>
+
+          <p>
+            Users can manage their own threads and own threadify account via
+            displaying their own threads and own account information.
+          </p>
+        </div>
+
         <button
           className="navigate-signUp"
           onClick={() => navigate("/sign-up")}
